119dc3ed68ba5368b3aa77ecb4870989510319d3
ViridRainDrop <siyanlia@gmail.com>
2025-04-28 03:05:11 -0700

    Lab6.1

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,11 +1,30 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Siyan Li
+// Date: 04/28/2025
 
 // Constants
 
-// Functions
+// Variables
+myTransport = ["Toyota CH-R"," Bike"," Bus"];
 
+//Creat object for dream rider
+myDreamRide = {
+  make: "Nissan",
+  model: "GT-R SpecV",
+  color: "Black",
+  year: 2009,
+  age: function(){
+      return 2025 - this.year;
+  }
+}
+//output
+document.writeln("The kinds of transport I use: ", myTransport, "</br>");
+//This allow us to write object to the doc.
+document.writeln("My Dream Ride: <pre>",
+  JSON.stringify(myDreamRide, null, '\t'), "</pre>");
+
+
+myDreamRide.age()
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
