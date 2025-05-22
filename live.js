5bcffe4ed1e54279cfc608eff30f83a931b7d1b5
ViridRainDrop <siyanlia@gmail.com>
2025-05-22 14:27:37 -0700

    Lab12.1.1

--- live.js
+++ live.js
@@ -1,3 +1,10 @@
 $("#button-welcome").click(function(){
     let name = prompt("What's your name?");
 $("#welcome").html("Hello, " + name + "!");})
+
+
+for (i = 1; 1<= 100; i++){
+if (1%15 == 0) {console.log("FizzBuzz");}
+else if (i%3 == 0) {console.log("Fizz");}
+else if (1%5 == 0) {console.log("Buzz");}
+else {console.log(i);} }
