ee7569fb4dfe80a6a5c551828980bd98b91164a1
ViridRainDrop <siyanlia@gmail.com>
2025-05-25 02:17:58 -0700

    Lab13.1

--- live.js
+++ live.js
@@ -3,8 +3,15 @@ $("#button-welcome").click(function(){
 $("#welcome").html("Hello, " + name + "!");})
 
 
-for (i = 1; 1<= 100; i++){
-if (1%15 == 0) {console.log("FizzBuzz");}
-else if (i%3 == 0) {console.log("Fizz");}
-else if (1%5 == 0) {console.log("Buzz");}
-else {console.log(i);} }
+function FizzBuzz() {
+    for (i = 1; 1<= 200; i++){
+    if (i%35 == 0) {console.log("BuzzBoom!");}
+    else if (i%21 == 0) {console.log("FizzBoom!");}
+    else if (i%15 == 0) {console.log("FizzBuzz!");}
+    else if (i%3 == 0) {console.log("Fizz!");}
+    else if (i%5 == 0) {console.log("Buzz!");}
+    else if (i%7 == 0) {console.log("Boom!")}
+    else {console.log(i);} 
+    $("#output").append("<p>" + str + "</p>");
+}
+}
